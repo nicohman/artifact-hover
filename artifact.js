4fe22888e48f8c772dde764fdcb34b0db4d12b8b
nicohman <nicohman@demenses.net>
2018-11-09 18:40:33 -0800

    Switch to new valve API for cards

--- artifact.js
+++ artifact.js
@@ -7,23 +7,20 @@ window.addEventListener("load", function(){
       return response.json();
     })
     .then(function(cards) {
-      console.log(cards);
       var comments = document.getElementsByClassName("usertext-body");
-      console.log(comments);
       Array.prototype.forEach.call(comments, function(comment) {
         Object.keys(cards).forEach(function(name) {
-          var nameReg = new RegExp(cards[name].Name,"i");
+          var nameReg = new RegExp(cards[name].card_name.english,"i");
           var i = comment.innerHTML;
           i = i.replace(nameReg, "<span style='font-weight:bold;color:blue;cursor:pointer' data-card='"+name+"' class='artifact-card'>$&</span>");
           comment.innerHTML = i;
         });
       });
-      var cards = document.getElementsByClassName("artifact-card");
-      console.log(cards);
+      var cardsi = document.getElementsByClassName("artifact-card");
       var dstyle = "position:fixed;width:auto;height:auto;left:50%;top:50%;z-index:1;transform: translate(-50%, -50%);"
-      Array.prototype.forEach.call(cards, function (card) {
-        var image =  document.createElement("img");
-        image.src = "https://www.artifactfire.com/images/cards/"+card.attributes["data-card"].nodeValue.toLowerCase().replace("'","").replace("_","-")+".jpg";
+      Array.prototype.forEach.call(cardsi, function (card) {
+        var image =  document.createElement("img")
+        image.src = cards[processCardName(card.attributes["data-card"].nodeValue)]["large_image"].default;
         image.style = dstyle+"display:none;";
         document.body.appendChild(image);
         card.addEventListener("mouseover", function (e) {
--- process_cards.js
+++ process_cards.js
@@ -1,15 +1,13 @@
-var manifest = require("./ArtifactDB/cards-manifest.json");
+var manifest = require("./artifact2.json");
 var fs = require("fs");
 var cards = {};
 function processCardName(name){
   return name.replace(" ","_").replace("'","-");
 }
-manifest.Sets.forEach(function(set){
-  set.Cards.forEach(function (card) {
-    cards[processCardName(card.Name)] = card;
-  })
+manifest.card_set.card_list.forEach(function(card){
+  cards[processCardName(card.card_name.english)] = card;
 });
-fs.writeFile("cards.json", JSON.stringify(cards), function (err) {
+fs.writeFile("cards2.json", JSON.stringify(cards), function (err) {
   if (err){
     console.log("Something went wrong.");
     throw err;
